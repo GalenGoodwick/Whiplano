082b012f977cd74410e5a9806a196c4f9235190f
Dan1718 <danielvincent1718@gmail.com>
2024-10-05 13:11:17 +0530

    Figuring out whats wrong with approve. Again ik :( #2

--- backend/mint.py
+++ backend/mint.py
@@ -54,8 +54,8 @@ def run_mint_script(image_path, metadata_path, name):
         return None
 
 async def mint(title,description,number,owner_email):
-    image_path = f'../tmp/collections/{title}/thumbnail'
-    metadata_path = f'../tmp/collections/{title}/metadata.json'
+    image_path = f'/tmp/collections/{title}/thumbnail'
+    metadata_path = f'/tmp/collections/{title}/metadata.json'
     download_file(f'trs_data/{title}/thumbnail.png', image_path)
     with open(metadata_path, 'w') as json_file:
         metadata = {
