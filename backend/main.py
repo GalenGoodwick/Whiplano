c3a4552fe47c730be85590144edbae4f8c312b82
Dan1718 <danielvincent1718@gmail.com>
2024-10-06 22:53:47 +0530

    bug fix #19

--- backend/main.py
+++ backend/main.py
@@ -281,13 +281,14 @@ async def admin_creation_requests():
 
 @app.post("/admin/approve",dependencies = [Depends(get_current_user)],tags = ["Admin"],summary = "For approving TRS creation requests, and minting the TRS")
 async def admin_approve(id: int):
-    exist = await database_client.check_collection_exists()
-    if exist: 
-        raise HTTPException(status_code= 409, content = "Collection already exists.")
+
     
     number = 1000
     trs_creation_data = await database_client.get_trs_creation_data(id)
     trs_creation_data = trs_creation_data[0]
+    exist = await database_client.check_collection_exists(trs_creation_data['title'])
+    if exist: 
+        raise HTTPException(status_code= 409, content = "Collection already exists.")   
     mint_address = await mint.mint(trs_creation_data['title'],trs_creation_data['description'],number,trs_creation_data['creator_email'])
     token_account_address = await transaction_module.get_token_account_address(Pubkey.from_string(mint_address))
     await database_client.approve_trs_creation_request(id,trs_creation_data['creator_email'],number,mint_address,trs_creation_data['title'],token_account_address)
